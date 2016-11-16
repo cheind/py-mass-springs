06debe6f4aa18ad4140e282d4f1bd7f50bf715e7
Christoph Heindl <christoph.heindl@gmail.com>
2016-11-16 19:18:37 +0100

    simplifications to the code

--- sympy_helpers.py
+++ sympy_helpers.py
@@ -1,23 +1,20 @@
 
 import sympy
 
-def symidx(idx, prefix='m_', offset=1):
-    if len(idx) == 1:
-        return sympy.symbols('{}{}'.format(prefix, idx[0] + offset))
-    else:
-        return sympy.symbols('{}{}{}'.format(prefix, idx[0] + offset, idx[1] + offset))
+def symidx(row, col, prefix='m_', offset=1):
+    return sympy.symbols('{}{}{}'.format(prefix, row + offset, col + offset))
 
 def symmat(rows, cols, prefix='m', type='dense'):
     if type == 'dense':
         syms = []
         for i in range(rows):
             for j in range(cols):
-                syms.append(symidx((i,j), prefix))
+                syms.append(symidx(i, j, prefix))
         return sympy.Matrix(rows, cols, syms)
     elif type == 'symmetric':
         m = sympy.Matrix.zeros(rows, rows)
         for i in range(rows):
             for j in range(i, rows):
-                sym = symidx((i,j), prefix)
+                sym = symidx(i, j, prefix)
                 m[i, j] = m[j, i] = sym
         return m
